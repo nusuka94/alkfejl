bcb32a6314f7182f9d832ae35278b2ddcc26cecf
nusuka94 <suszter.anna@gmail.com>
2016-09-28 12:56:14 +0200

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,13 @@
+
+function waitFor(ms) {
+    return new Promise(function(resolve,reject) {
+        setTimeout(resolve,ms);
+    });
+}
+
+waitFor(1000)
+    .then(() => {
+        return 7
+    })
+    .then(foo => console.log(foo))
+console.log(2)
